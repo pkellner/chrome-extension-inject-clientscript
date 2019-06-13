2e8fdf050eca7e3b470fd9a4e16cdf687ef0bac8
pkellner <peter@peterkellner.net>
2019-06-13 12:50:12 -0700

    wip

--- content-script.js
+++ content-script.js
@@ -4,22 +4,43 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       ? "from a content script:" + sender.tab.url
       : "from the extension"
   );
-  if (request.greeting == "hello") sendResponse({ farewell: "goodbye" });
+
+  // if (request.greeting == "hello") sendResponse({ farewell: "goodbye" });
+  document.addEventListener("jquery-version", onVersionRecieved, {
+    once: true
+  });
+
+  function onVersionRecieved(event) {
+    if (event.detail) {
+      alert(`content-scriptx: Return From inject-script ${event.detail}`);
+    } else {
+      alert(`content-scriptx: Return From inject-script  no event.detail`);
+    }
+    if (request.greeting == "hello")
+      sendResponse({ farewell: "goodbye " + event.detail });
+  }
+
+  const versionScript = document.createElement("script");
+  versionScript.src = chrome.runtime.getURL("inject-script.js");
+  versionScript.onload = function autoUnload() {
+    this.remove;
+  };
+  document.body.appendChild(versionScript);
 });
 
-document.addEventListener("jquery-version", onVersionRecieved, { once: true });
-
-function onVersionRecieved(event) {
-  if (event.detail) {
-    alert(`content-script: Return From inject-script ${event.detail}`);
-  } else {
-    alert(`content-script: Return From inject-script  no event.detail`);
-  }
-}
-
-const versionScript = document.createElement("script");
-versionScript.src = chrome.runtime.getURL("inject-script.js");
-versionScript.onload = function autoUnload() {
-  this.remove;
-};
-document.body.appendChild(versionScript);
+// document.addEventListener("jquery-version", onVersionRecieved, { once: true });
+//
+// function onVersionRecieved(event) {
+//   if (event.detail) {
+//     alert(`content-script: Return From inject-script ${event.detail}`);
+//   } else {
+//     alert(`content-script: Return From inject-script  no event.detail`);
+//   }
+// }
+//
+// const versionScript = document.createElement("script");
+// versionScript.src = chrome.runtime.getURL("inject-script.js");
+// versionScript.onload = function autoUnload() {
+//   this.remove;
+// };
+// document.body.appendChild(versionScript);
